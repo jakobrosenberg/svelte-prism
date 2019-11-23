a6058828396d47fcd617bbbe142213545e8620fa
jakobrosenberg <jakobrosenberg@gmail.com>
2019-11-23 14:47:49 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # svelte-prism
+
+
+### Install
+``npm i svelte-prism``
+
+
+### Usage
+```html
+<script>
+    import Prism from 'svelte-prism'
+</script>
+
+<!-- Option one -->
+<Prism language="html" source="{mycode}" />
+
+<!-- Options two -->
+<Prism language="html">
+    {mycode}
+</Prism>
+```
+
+
+### Themes
+To use a theme, import the CSS to your project. Ie.
+``<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-okaida.min.css'>``
+
+More themes can be found here
+https://cdnjs.com/libraries/prism
---
+++ src/index.svelte
@@ -0,0 +1,30 @@
+<script>
+  import prism from "prismjs";
+  import { tick } from "svelte";
+  export let language = "javascript",
+    source = "";
+  let element, formattedCode;
+
+  $: $$props && (source || element) && highlightCode();
+
+  async function highlightCode() {
+    await tick();
+    const grammar = Prism.languages[language];
+    let body = element.textContent;
+    formattedCode = Prism.highlight(body, grammar, language);
+  }
+
+  function attachElement(e) {
+    element = e;
+  }
+</script>
+
+<code style="display:none" use:attachElement>
+  <slot />
+</code>
+
+<pre class="language-{language}" style="border-radius: 0; margin: 0;">
+  <code class="language-{language}">
+    {@html formattedCode}
+  </code>
+</pre>
