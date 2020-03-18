df51f233b7112fda712c4bfeeed7277a8ff3a38e
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-18 12:28:27 +0100

    cleanup

--- src/Prism.svelte
+++ src/Prism.svelte
@@ -16,21 +16,16 @@
 
   $: $$props && (source || element) && highlightCode();
 
-  async function highlightCode() {
-    await tick();
+  function highlightCode() {    
     const grammar = Prism.languages[language];
     let body = source || element.textContent;
     body = global.transform(body);
     body = transform(body);
     formattedCode = Prism.highlight(body, grammar, language);
   }
-
-  function attachElement(e) {
-    element = e;
-  }
 </script>
 
-<code style="display:none" use:attachElement>
+<code bind:this={element} style="display:none">
   <slot />
 </code>
 
