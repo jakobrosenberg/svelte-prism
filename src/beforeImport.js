5f6ab7e4c128c1564af41e4af20583566bbc4457
Ray Thurne Void <53383860+raythurnevoid@users.noreply.github.com>
2022-05-02 07:24:07 +0200

    fix: safe window check for ssr (#35)

--- src/beforeImport.js
+++ src/beforeImport.js
@@ -1,5 +1,9 @@
-if (window.Prism)
-    console.warn('Prism has already been initiated. Please ensure that svelte-prism is imported first.')
+if (typeof window !== "undefined") {
+	if (window.Prism)
+		console.warn(
+			"Prism has already been initiated. Please ensure that svelte-prism is imported first."
+		);
 
-window.Prism = window.Prism || {}
-window.Prism.manual = true
+	window.Prism = window.Prism || {};
+	window.Prism.manual = true;
+}
